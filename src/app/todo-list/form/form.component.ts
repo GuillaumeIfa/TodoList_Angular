4261a464534396ec4872c9d81c5f65dbd37eab56
GuillaumeIfa <ory.guillaume@stagiairesifa.fr>
2019-05-23 11:26:55 +0200

    Nettoyage code

--- src/app/todo-list/form/form.component.ts
+++ src/app/todo-list/form/form.component.ts
@@ -3,34 +3,34 @@ import { todo } from 'src/app/interfaces/todo.interface';
 import { TodoService } from 'src/app/todoList.service';
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+	selector: 'app-form',
+	templateUrl: './form.component.html',
+	styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
 
-  text:string;
-  desc:string;
-  done:boolean = false;
-  test:todo;
+	text:string;
+	desc:string;
+	done:boolean = false;
+	test:todo;
 
   constructor(private todoServ: TodoService) { }
 
-  envoyer() {
-    if(this.text){
-      this.test = {
-        text: this.text,
-        description: this.desc,
-        done: this.done 
-      }
-      this.todoServ.setNewTodo(this.test);
-      this.text = "";
-      this.desc = "";
-      this.done = false;
-    }
-  }
+	envoyer() {
+		if(this.text){
+			this.test = {
+			text: this.text,
+			description: this.desc,
+			done: this.done 
+			}
+			this.todoServ.setNewTodo(this.test);
+			this.text = "";
+			this.desc = "";
+			this.done = false;
+		}
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -3,38 +3,38 @@ import { todo } from '../interfaces/todo.interface';
 import { TodoService } from '../todoList.service';
 
 @Component({
-  selector: 'app-todo-list',
-  templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+	selector: 'app-todo-list',
+	templateUrl: './todo-list.component.html',
+	styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
-  
-  toggleInput:boolean = false;
+	
+	toggleInput:boolean = false;
 
-  todo:todo;
-  index:number;
-  todolist: Array<todo> = [];
+	todo:todo;
+	index:number;
+	todolist: Array<todo> = [];
 
-  constructor(private todoServ: TodoService) { }
+	constructor(private todoServ: TodoService) { }
 
 
-  supprimer(index: number) {
+	supprimer(index: number) {
 	this.todoServ.deleteTodo(index);
-  }
-  
-  toInput( index: number) {
+	}
+	
+	toInput( index: number) {
 	this.index = index;
 	this.toggleInput = true;
-  }
+	}
 
-  toggleInputFalse() {
+	toggleInputFalse() {
 	// this.todoServ.setNewTodo();
 	this.toggleInput = false;
 	this.index = null;
-  }
+	}
 
-  ngOnInit() {
+	ngOnInit() {
 	this.todolist = this.todoServ.getTodolist();
-  }
+	}
 
 }
